3ca137e346ae86e17d2bbd71cf6a446e9f062902
AnaJaolino <ana.jaolino@gmail.com>
2021-04-15 20:54:50 -0300

    Update matriz_vetor_fortran.f95

--- matriz_vetor_fortran.f95
+++ matriz_vetor_fortran.f95
@@ -1,56 +1,56 @@
 program matriz_vetor_f
     implicit none
 
-    character(len=32) :: arg
+    character(len = 32) :: arg
     integer :: n, func
     real(8) :: inicio, fim
-    real(8), dimension(:,:), allocatable :: matriz
+    real(8), dimension (:,:), allocatable :: matriz
     real(8), dimension (:), allocatable :: vetor
     real(8), dimension (:), allocatable :: resultado
 
 
-    call getarg(1, arg)    
+    call getarg (1, arg)    
     read(arg, "(I10)") n
 
 
-    call getarg(2, arg)    
+    call getarg (2, arg)    
     read(arg, "(I1)") func
 
 
-    allocate(matriz(n, n))
-    allocate(vetor(n))
-    allocate(resultado(n))
+    allocate (matriz(n, n))
+    allocate (vetor(n))
+    allocate (resultado(n))
 
     call random_seed()
 
-    call gerar_vetor(vetor, n)
-    call gerar_matriz(matriz, n)
+    call gerar_vetor (vetor, n)
+    call gerar_matriz (matriz, n)
 
     if (func == 1) then
         call cpu_time(inicio) 
-        call produto_linha_coluna(vetor, matriz, resultado, n)
+        call produto_linha_coluna (vetor, matriz, resultado, n)
         call cpu_time(fim) 
     else if (func == 0) then
         call cpu_time(inicio) 
-        call produto_coluna_linha(vetor, matriz, resultado, n)
+        call produto_coluna_linha (vetor, matriz, resultado, n)
         call cpu_time(fim) 
     else
         CALL EXIT(0)
     end if
 
 
-    deallocate(matriz)
-    deallocate(vetor)
-    deallocate(resultado)
+    deallocate (matriz)
+    deallocate (vetor)
+    deallocate (resultado)
 
     print *, n, ";", (fim - inicio)
 
     contains
 
-    subroutine gerar_vetor(vetor, n)
+    subroutine gerar_vetor (vetor, n)
         implicit none
 
-        real(8), dimension(:) :: vetor
+        real(8), dimension (:) :: vetor
         integer :: n, linha
         real(8) :: valor
         do linha = 1, n
@@ -59,10 +59,10 @@ program matriz_vetor_f
         end do 
     end
 
-    subroutine gerar_matriz(matriz, n)
+    subroutine gerar_matriz (matriz, n)
         implicit none
 
-        real(8), dimension(:,:) :: matriz
+        real(8), dimension (:,:) :: matriz
         integer :: n, linha, coluna
         real(8) :: valor
 
@@ -77,11 +77,10 @@ program matriz_vetor_f
     subroutine zerar_resultado (vetor, n)
         implicit none
 
-        real(8), dimension(:) :: vetor
+        real(8), dimension (:) :: vetor
         integer :: n, linha
         real(8) :: number
         do linha = 1, n
-            call random_number(valor)
             vetor(linha) = 0
         end do 
     end
@@ -94,7 +93,7 @@ program matriz_vetor_f
         real(8), dimension (:) :: resultado
         integer :: linha, coluna, n
 
-        call zerar_resultado(resultado, n)
+        call zerar_resultado (resultado, n)
 
         do coluna=1, n
             do linha=1, n
@@ -111,7 +110,7 @@ program matriz_vetor_f
         real(8), dimension (:) :: resultado
         integer :: linha, coluna, n
 
-        call zerar_resultado(resultado, n)
+        call zerar_resultado (resultado, n)
 
         do linha=1, n
             do coluna=1, n
